5e621193170dd8e5db81f1d10e4fa5c844c7c6d1
Vitaly Ivanov <vitaly.ivanov@castlabs.com>
2021-11-21 17:39:34 +0700

    board clicks, black/white cells

--- js/main.js
+++ js/main.js
@@ -1,16 +1,47 @@
 const boardWidth = 15;
 const boardHeight = 15;
 
-function NewGame() {
+let boardArray;
+
+function UpdateBoard() {
+    document.body.innerHTML = '';
+
     const board = document.createElement('div');
     board.className = 'square-container';
     board.style.setProperty('--grid-cols', boardWidth);
-    for (let c = 0; c < (boardWidth * boardHeight); ++c) {
-        const cell = document.createElement('div');
-        cell.className = 'square';
-        cell.innerText = ' ';
-        cell.style.pointerEvents = 'auto';
-        board.appendChild(cell);
+
+    for (let row = 0; row < boardHeight; ++row) {
+        for (let col = 0; col < boardWidth; ++col) {
+            const cell = document.createElement('div');
+            cell.className = 'square';
+            if (boardArray[row][col] === 1) {
+                cell.style.backgroundColor = 'black';
+                cell.style.pointerEvents = 'none';
+            }
+            else if (boardArray[row][col] === -1) {
+                cell.style.backgroundColor = 'white';
+                cell.style.pointerEvents = 'none';
+            }
+            else {
+                cell.addEventListener('click', function(){
+                    SquareClicked(row, col);
+                });
+            }
+            board.appendChild(cell);
+        }
     }
+
     document.body.appendChild(board);
+}
+
+function NewGame() {
+    boardArray = Array.from(Array(boardHeight), () => new Array(boardWidth));
+    UpdateBoard();
+}
+
+let currColor = 1;
+function SquareClicked(row, col) {
+    boardArray[row][col] = currColor;
+    currColor = -currColor;
+    UpdateBoard();
 }
